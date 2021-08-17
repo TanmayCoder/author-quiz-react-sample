c0bae12f85ba2d6ab61722a8360f4405ed1abd4f
TanmayCoder <tanmaydeveloper@gmail.com>
2021-08-18 00:27:18 +0530

    1 file modified

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,11 +2,6 @@ import React from 'react';
 import './style.css';
 
 function AuthorQuiz() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return <div>Author Quiz</div>;
 }
 export default AuthorQuiz;
