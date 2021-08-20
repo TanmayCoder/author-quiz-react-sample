b0341e7d5f784b6d2c46c98bc1dbda4ec7db8eb8
tanmaydeveloper <tanmaydeveloper@gmail.com>
2021-08-20 15:43:08 +0530

    sample initial draft

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,7 +2,46 @@ import React from 'react';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function AuthorQuiz() {
-  return <div>Author Quiz</div>;
+function Header(){
+  return (<div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown</p>
+    </div>
+  </div>);
+}
+function Book({title}){
+  return(<div className="answer">
+    <h4>{title}</h4>
+  </div>);
+
+}
+function Turn({author, books}){
+  return (<div className="row turn" style={{backgroundColor:"white"}}>
+    <div className="col-4 offset-1">
+      <img src={author.imageUrl} className="authorimage" alt="Author"/>
+    </div>
+    <div className="col-6">
+      {books.map((title)=><Book title={title} key={title}/>)}
+    </div>
+  </div>);
+}
+function Continue(){
+  return <div/>
+}
+function Footer(){
+  return (<div className="row">
+    <div className="col-12">
+      <p className="text-muted credit">All image are from <a href="https://commons.wikimedia.org/wiki/Main_Page">Wikipedia Commons </a>and are in public domains</p>
+    </div>
+  </div>);
+}
+function AuthorQuiz({turnData}) {
+  return <div className="container-fluid">
+    <Header/>
+    <Turn {...turnData}/>
+    <Continue/>
+    <Footer/>
+  </div>;
 }
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+import { shuffle,sample } from 'underscore';
+const authors=[
+    {name:"Mark Twain",
+    imageUrl:'images/marktwain.jpg',
+    imageSource:"Wikipedia Commons",
+    books:["The Adventures of Huckleberry Finn",'Life On Missisipi','Roughing It']
+    },
+    {name:"Joseph Conrad",
+    imageUrl:'images/josephconrad.png',
+    imageSource:"Wikipedia Commons",
+    books:['Heart of Darknes']
+    },
+    {name:"J. K. Rowling",
+    imageUrl:'images/jkrowling.jpg',
+    imageSource:"Wikipedia Commons",
+    books:['Harry Potter and the Sorcerers Stone']
+    },
+    {name:"Stephen King",
+    imageUrl:'images/stephenking.jpg',
+    imageSource:"Wikipedia Commons",
+    books:['The Shining','IT']
+    },
+    {name:"Carles Dickens",
+    imageUrl:'images/charlesdickens.jpg',
+    imageSource:"Wikipedia Commons",
+    books:['David Copperfield','A Tale of Two Cities']
+    },
+    {name:"William Shakespeare",
+    imageUrl:'images/shakespeare.jpg',
+    imageSource:"Wikipedia Commons",
+    books:['Hamlet', 'Macbeth','Romeo and Juliet']
+    }
+];
+function getTurnData(authors){
+
+    const allBooks=authors.reduce(function(p,c,i){
+        return p.concat(c.books);
+    },[]);
+    const fourRandomBooks = shuffle(allBooks).splice(0,4);
+    const answer = sample(fourRandomBooks);
+    return {
+        books:fourRandomBooks,
+        author:authors.find((author)=> author.books.some((title)=>title===answer))
+    }
+}
+
+const state={
+    turnData:getTurnData(authors)
+}
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
